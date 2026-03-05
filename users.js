abb6150a35dc74db4fdf9f98774943f93658c031
harixxonn <piotr.nowo09@gmail.com>
2026-03-05 22:08:35 +0100

    Add files via upload

---
+++ users.js
@@ -0,0 +1,9 @@
+// ============================================
+//  LISTA UŻYTKOWNIKÓW — edytuj tutaj
+//  Dodaj nowy wpis: { username: "...", password: "..." }
+// ============================================
+
+const USERS = [
+    { username: "hrxn444",   password: "admin123"    },
+    { username: "skute_korko",   password: "jabuszko67"  },
+];
