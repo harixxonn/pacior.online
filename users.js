cef33347056f6c22b0e857c2cbe5f106c02c3116
harixxonn <piotr.nowo09@gmail.com>
2026-03-10 00:30:50 +0100

    Update users.js

--- users.js
+++ users.js
@@ -4,7 +4,8 @@
 // ============================================
 
 const USERS = [
-    { username: "hrxn444",   password: "admin123"    },
+    { username: "hrxn444",   password: "admin123", verified: "elite"    },
     { username: "skute_korko",   password: "jabuszko67"  },
-    { username: "szepcze",   password: "krzycze"  }
+    { username: "szepcze",   password: "krzycze"  },
+    { username: "Epstein",   password: "epsteinbot", verified: "bot" }
 ];
